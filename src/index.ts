736dc1cb6071d783933ac7a07e00b7bbcc21e0a5
tran kevin <kevintran11082002@gmail.com>
2024-10-04 16:52:39 -0400

    add few more services for the company

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 import { createServer } from 'node:http'
 import { Request, Response } from 'express'
 
-// Require dependecies
+// Require dependencies
 const express = require('express')
 const cors = require('cors')
 const app = express()
@@ -44,21 +45,73 @@ app.post("/api/v1/users/register-user", (req: Request, res: Response) => {
   res.status(200)
   res.end("Successfully registered user")
 })
+//sign in with google
+app.post("/api/v1/users/create-user-with-google", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully signed in with google")
+})
+
+//sign in
+app.post("/api/v1/users/login", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully signed in")
+})
 
 /************ Device Services ************/
 app.get("/api/v1/devices/get-device", (req: Request, res: Response) => {
   res.setHeader('Content-Type', 'text/plain')
   res.status(200)
-  res.end("Successflly found device")
+  res.end("Successfully found device")
+})
+// Create Device
+app.post("/api/v1/devices/create-device", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully created device")
 })
 
+//edit device
+app.put("/api/v1/devices/update-device", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully edited device")
+})
+
+//edit group
+app.put("/api/v1/groups/update-group", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully edited group")
+})
+
+//delete group
+app.delete("/api/v1/groups/delete-group", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully deleted group")
+})
+
+//delete device
+app.delete("/api/v1/devices/delete-device", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully deleted device")
+})
 /************ Group Services ************/
 app.get("/api/v1/groups/get-group", (req: Request, res: Response) => {
   res.setHeader('Content-Type', 'text/plain')
   res.status(200)
-  res.end("Successflly found group")
+  res.end("Successfully found group")
 })
 
+//create group
+app.post("/api/v1/groups/create-group", (req: Request, res: Response) => {
+  res.setHeader('Content-Type', 'text/plain')
+  res.status(200)
+  res.end("Successfully created group") 
+})
 // Initialize the server
 const server = createServer(app)
 
