def89658bc428bdf465b4c746d8e371ddcefbb88
Tanner51 <148127266+Tanner51@users.noreply.github.com>
2024-10-16 14:14:19 -0400

    Update GroupService.ts

--- src/Services/GroupService.ts
+++ src/Services/GroupService.ts
@@ -4,22 +4,22 @@ import { Request, Response } from 'express';
  * GroupService.ts
  */
 export class GroupService {
-    async update-group(req: Request, res: Response) {
+    async updateGroup(req: Request, res: Response) {
         res.setHeader('Content-Type', 'text/plain')
         res.status(200)
         res.end("Successfully edited group")
     }
-    async delete-group(req: Request, res: Response) {
+    async deleteGroup(req: Request, res: Response) {
         res.setHeader('Content-Type', 'text/plain')
         res.status(200)
         res.end("Successfully deleted group")
     }
- async get-group(req: Request, res: Response) {
+ async getGroup(req: Request, res: Response) {
         res.setHeader('Content-Type', 'text/plain')
         res.status(200)
         res.end("Successfully found group")
     }
- async create-group(req: Request, res: Response) {
+ async createGroup(req: Request, res: Response) {
         res.setHeader('Content-Type', 'text/plain')
         res.status(200)
         res.end("Successfully created group")
