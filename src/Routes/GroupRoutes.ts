e965c41a5449ac3c240a65cf578d85e7db8ea123
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-10-16 14:14:45 -0400

    Update GroupRoutes.ts

--- src/Routes/GroupRoutes.ts
+++ src/Routes/GroupRoutes.ts
@@ -4,9 +4,9 @@ import { GroupService } from '../Services/GroupService'
 const router = express.Router()
 const groupService = new GroupService()
 
-router.put('/update-group', groupService.update-group.bind(userService))
-router.delete('/delete-group', groupService.delete-group.bind(userService))
-router.get('/get-group', groupService.get-group.bind(userService))
-router.post('/create-group', groupService.create-group.bind(userService))
+router.put('/update-group', groupService.update-group.bind(groupService))
+router.delete('/delete-group', groupService.delete-group.bind(groupService))
+router.get('/get-group', groupService.get-group.bind(groupService))
+router.post('/create-group', groupService.create-group.bind(groupService))
 
 module.exports = router
