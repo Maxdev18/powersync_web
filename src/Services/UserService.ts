e5019ea3be7b71ee6b6f5db5be4122123b5dac92
Max Melnik <maxpersonal1721@gmail.com>
2024-10-24 12:14:24 -0400

    User registration service complete along with password hashing

--- src/Services/UserService.ts
+++ src/Services/UserService.ts
@@ -1,24 +1,43 @@
 import { Request, Response } from 'express';
 import User from '../Schemas/UserSchema';
+import bcrypt from 'bcrypt'
 
 /**
  * UserService.ts
  */
 export class UserService {
     async registerUser(req: Request, res: Response) {
-        const user = new User({
-            firstName: "Max",
-            lastName: "Melnik",
-            email: "mzm6958@psu.edu",
-            password: "password123",
-            theme: "dark"
-        })
+        const data = req.body
 
-        await user.save()
-        
-        res.setHeader('Content-Type', 'text/plain')
-        res.status(200)
-        res.end("Successfully registered user")
+        res.setHeader('Content-Type', 'application/json')
+
+        try {
+            // Check if account already exists
+            const userDoc = await User.findOne({ email: data.email })
+            
+            if(userDoc) {
+                res.status(409).json({ message: "User already exists" }).end()
+            } else {
+                // Encrypt password
+                const encryptedPassword = await bcrypt.hash(data.password, bcrypt.genSaltSync(), null)
+
+                const user = new User({
+                    firstName: data.firstName,
+                    lastName: data.lastName,
+                    email: data.email,
+                    password: encryptedPassword,
+                    theme: data.theme
+                })
+
+                await user.save()
+
+                res.status(200).json({ message: "Successfully added user to database" }).end()
+            }
+        } catch(error) {
+            console.log(error)
+            res.status(500)
+            res.json({ message: "Internal server error" }).end()
+        }   
     }
 
     async loginUser(req: Request, res: Response) {
