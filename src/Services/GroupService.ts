63f02166fe31054131e4d9e4d0ae2a9591f57eaf
lockup62 <36246100+lockup62@users.noreply.github.com>
2024-10-25 10:55:01 -0400

    Update GroupService.ts updateGroup

--- src/Services/GroupService.ts
+++ src/Services/GroupService.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from 'express';
-
+import Group from '../Schemas/GroupSchema
+import bcrypt from 'bcrypt'
 /**
  * GroupService.ts
  */
 export class GroupService {
     async updateGroup(req: Request, res: Response) {
-        res.setHeader('Content-Type', 'text/plain')
-        res.status(200)
-        res.end("Successfully edited group")
+        const data = req.body;
+        res.setHeader('Content-Type', 'application/json');
+        try {
+            const groupDoc = await Group.findOneAndUpdate({ name: data.name }, { name: data.newName }, { new: true });
+            if (groupDoc) {
+                res.status(200).json({ message: "Successfully updated group", group: groupDoc }).end();
+            } else {
+                res.status(404).json({ message: "Group not found" }).end();
+            }
+        } catch (error) {
+            console.log(error);
+            res.status(500).json({ message: "Internal server error" }).end();
+        }
     }
+}
     async deleteGroup(req: Request, res: Response) {
         res.setHeader('Content-Type', 'text/plain')
         res.status(200)
