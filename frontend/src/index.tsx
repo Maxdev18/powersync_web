220f664a62133c1b4ad1276b817767acf15f10d4
Max Melnik <maxpersonal1721@gmail.com>
2024-11-07 10:55:30 -0500

    Added authentication APIs and types

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Axios from 'axios'
+
+Axios.defaults.baseURL = "http://localhost:8080"
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
