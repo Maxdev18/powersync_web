910b3f3d80a316b60bd565923a504a6e56fa1268
Maxim Melnik <maxim.melnikwork@gmail.com>
2024-11-09 11:42:11 -0500

    Added user APIs

---
+++ frontend/src/APIs/User.ts
@@ -0,0 +1,65 @@
+import Axios from 'axios'
+import { Response } from '../Types/Response'
+import { User } from '../Types/User'
+
+export class UserAPI {
+  static async login(credentials: { email: string, password: string }): Promise<Response> {
+    return Axios.post('/api/v1/users/login', credentials)
+      .then(res => {
+        console.log(res)
+        return { message: res.data.message, data: res.data.data, isError: false }
+      })
+      .catch(err => {
+        console.log(err.data)
+        return err.data as Response
+      })
+  }
+
+  static async register(credentials: User): Promise<Response> {
+    return Axios.post('/api/v1/users/register-user', credentials)
+      .then(res => {
+        console.log(res)
+        return { message: res.data.message, data: res.data.data, isError: false }
+      })
+      .catch(err => {
+        console.log(err.data)
+        return err.data as Response
+      })
+  }
+
+  static async updateUser(user: User): Promise<Response> {
+    return Axios.put('/api/v1/users/update-user', user)
+      .then(res => {
+        console.log(res)
+        return { message: res.data.message, data: res.data.data, isError: false }
+      })
+      .catch(err => {
+        console.log(err.data)
+        return err.data as Response
+      })
+  }
+
+  static async updatePassword(user: User): Promise<Response> {
+    return Axios.put('/api/v1/users/update-password', user)
+      .then(res => {
+        console.log(res)
+        return { message: res.data.message, data: res.data.data, isError: false }
+      })
+      .catch(err => {
+        console.log(err.data)
+        return err.data as Response
+      })
+  }
+
+  static async deleteUser(id: string): Promise<Response> {
+    return Axios.delete('/api/v1/users/delete-user', { params: { id } })
+      .then(res => {
+        console.log(res)
+        return { message: res.data.message, data: res.data.data, isError: false }
+      })
+      .catch(err => {
+        console.log(err.data)
+        return err.data as Response
+      })
+  }
+}
---
+++ frontend/src/Types/Response.ts
@@ -0,0 +1,5 @@
+export type Response = {
+  message: string
+  data?: object
+  isError: boolean
+}
