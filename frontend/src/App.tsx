36bbcfe88d3940dc3596b27ddca624da3f53ea54
KevVvTheDev <kevintran11082002@gmail.com>
2024-11-11 12:55:01 -0500

    fix background issue for login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,6 +10,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<LoginPage />} /> 
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
     
