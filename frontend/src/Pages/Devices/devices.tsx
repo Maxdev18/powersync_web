c53703138097e5dde4b4aa07402d8a40d10b93de
TannerL <tpl5459@psu.edu>
2024-12-13 17:56:49 -0500

    Edit device update

--- frontend/src/Pages/Devices/devices.tsx
+++ frontend/src/Pages/Devices/devices.tsx
@@ -151,7 +151,7 @@ function Dashboard() {
         <div className="mainContainer">
           <div className="locationConsumptionContainer">
             <div className="locationContainer">
-              <p> MAP WILL GO HERE DUHHH</p>
+              <p> MAP WILL GO HERE</p>
             </div>
             <div className="consumptionContainer">
               <p className="bolderFont">Consumption</p>
